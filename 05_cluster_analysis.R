70131c1be6752d0f36379cd7fb7e158e71965b79
Anastasia Lianguzova <anastasialianguzova@gmail.com>
2023-03-04 22:08:01 +0300

    added dendrogram coloring

--- 05_cluster_analysis.R
+++ 05_cluster_analysis.R
@@ -150,6 +150,33 @@ tanglegram(untang_w[[1]], untang_w[[2]],
            lab.cex = 1.5, cex_main = 2)
 
 
+# Раскраска дендрограмм ====================================
+
+# Раскраска происходит в порядке следования веток на дендрограмме
+
+# а) Вручную
+# Здесь в примере просто произвольные цвета
+cols <- rainbow(30)
+den_single_manual <- color_labels(dend = den_single, col = cols)
+plot(den_single_manual, horiz = TRUE)
+
+# б) При помощи функции
+# Функция для превращения лейблов в цвета
+# (группы определяются по `n_chars` первых букв)
+get_colours <- function(dend, n_chars, palette = "Dark2"){
+  labs <- get_leaves_attr(dend, "label")
+  group <- substr(labs, start = 0, stop = n_chars)
+  group <- factor(group)
+  cols <- brewer.pal(length(levels(group)), name = palette)[group]
+  return(cols)
+}
+
+# Применяем функцию
+library(RColorBrewer)
+cols <- get_colours(dend = den_single, n_chars = 3)
+den_single_c <- color_labels(dend = den_single, col = cols)
+plot(den_single_c, horiz = TRUE)
+
 
 ##
 webpage <- "http://evolution.genetics.washington.edu/book/primates.dna"
