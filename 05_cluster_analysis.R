59a3df1bc7dee56c42795e5667242e70a0c1185e
Anastasia Lianguzova <anastasialianguzova@gmail.com>
2023-03-05 13:31:08 +0300

    matched presentation and code for students

--- 05_cluster_analysis.R
+++ 05_cluster_analysis.R
@@ -1,4 +1,9 @@
-##### Загружаем данные ####
+# install.packages(c("candisc", "ape", "dendextend", "fpc", "pvclust"))
+
+## Пример: Волки ===========================================
+# Морфометрия черепов у волков в Скалистых горах и в Арктике (Jolicoeur, 1959)
+
+# Загружаем данные #
 library(candisc)
 data("Wolves")
 
@@ -10,8 +15,13 @@ head(rownames(Wolves))
 any(is.na(Wolves))
 table(Wolves$group)
 
+## Задание 1 -------------------------------------------------
+# - Постройте ординацию nMDS данных о морфометрии волков
+# - Оцените качество ординации
+# - Обоснуйте выбор коэффициента
+# - Раскрасьте точки на ординации волков в зависимости от
+# географического происхождения (`group`)
 
-##### Построение MDS-ординации ####
 library(vegan)
 library(ggplot2)
 theme_set(theme_bw(base_size = 16))
@@ -24,11 +34,14 @@ gg_w <- ggplot(dfr_w, aes()) +
   geom_point()
 
 
+# Кластерный анализ в R =====================================
+
 ##### Пакеты для визуализации кластеризации ####
 library(ape)
 library(dendextend)
 
 # Матрица расстояний
+st_w <- scale(Wolves[, 4:ncol(Wolves)]) ## стандартизируем
 d <- dist(x = st_w, method = "euclidean")
 
 
@@ -43,51 +56,62 @@ ph_single <- as.phylo(hc_single)
 plot(ph_single, type = "phylogram")
 axisPhylo()
 
+plot(ph_single, type = "phylogram")
+plot(ph_single, type = "cladogram")
+plot(ph_single, type = "fan")
+plot(ph_single, type = "unrooted")
+plot(ph_single, type = "radial")
+
+axisPhylo()
+
 
 # С помощью dendextend
 den_single <- as.dendrogram(hc_single)
-plot(den_single)
+plot(den_single, horiz = TRUE)
 
 
 ##### Метод отдалённого соседа ####
 
-# Ape
+# Визаулизация средства ape
 hc_compl <- hclust(d, method = "complete")
 ph_compl <- as.phylo(hc_compl)
 plot(ph_compl, type = "phylogram")
 axisPhylo()
 
 
-# Dendextend
+# Визуализация средствами dendextend
 den_compl <- as.dendrogram(hc_compl)
-plot(den_compl)
+plot(den_compl, horiz = TRUE)
 
 
-##
+##### Метод невзвешенного попарного среднего (UPGMA) ####
+
+# Средствами ape
 hc_avg <- hclust(d, method = "average")
 ph_avg <- as.phylo(hc_avg)
 plot(ph_avg, type = "phylogram")
 axisPhylo()
 
 
-##
+# Средствами dendextend
 den_avg <- as.dendrogram(hc_avg)
-plot(den_avg)
+plot(den_avg, horiz = TRUE)
 
 
-##
-hc_w2 <-hclust(d, method = "ward.D2")
+##### Метод Варда ####
+
+# Средствами ape
+hc_w2 <- hclust(d, method = "ward.D2")
 ph_w2 <- as.phylo(hc_w2)
 plot(ph_w2, type = "phylogram")
 axisPhylo()
 
-
-##
+# Средствами dendextend
 den_w2 <- as.dendrogram(hc_w2)
-plot(den_w2)
+plot(den_w2, horiz = TRUE)
 
 
-##
+## Кофенетическая корреляция ===============================
 # Матрица кофенетических расстояний
 c_single <- cophenetic(ph_single)
 
@@ -95,44 +119,41 @@ c_single <- cophenetic(ph_single)
 # = корреляция матриц кофенетич. и реальн. расстояний
 cor(d, as.dist(c_single))
 
-
-##
-cor(d, as.dist(c_single))
-
-c_compl <- cophenetic(ph_compl)
-cor(d, as.dist(c_compl))
-
-c_avg <- cophenetic(ph_avg)
-cor(d, as.dist(c_avg))
-
-c_w2 <- cophenetic(ph_w2)
-cor(d, as.dist(c_w2))
+## Задание 2 ------------------------------------------------
+# Оцените при помощи кофенетической
+# корреляции качество кластеризаций, полученных разными
+# методами.
+# Какой метод дает лучший результат?
 
 
-##
+
+# Качество и количество кластеров ==========================
+
+## Ширина силуэта ==========================================
+
+# Оценка ширины силуэта для 3 кластеров
 library(cluster)
 complete3 <- cutree(tree = hc_avg, k = 3)
-plot(silhouette(x = complete3, dist = d), cex.names = 0.6)
+plot(silhouette(x = complete3, dist = d), cex.names = x.names = 0.6)
 
 
-##
+## Бутстреп поддержка ветвей ===============================
 library(pvclust)
 # итераций должно быть 1000 и больше, здесь мало для скорости
 cl_boot <- pvclust(t(st_w), method.hclust = "average", nboot = 100,
                    method.dist = "euclidean", parallel = TRUE, iseed = 42)
 
 
-##
 plot(cl_boot)
 pvrect(cl_boot) # достоверные ветвления
 
 
-##
+## Для диагностики качества оценок AU
 seplot(cl_boot)
 print(cl_boot) # все значения
 
 
-##
+##### Танглграмма ####
 set.seed(395)
 untang_w <- untangle_step_rotate_2side(den_compl, den_w2, print_times = F)
 
@@ -148,6 +169,11 @@ tanglegram(untang_w[[1]], untang_w[[2]],
            margin_inner = 4, margin_outer = 0.5,
            lwd = 1.2, edge.lwd = 1.2,
            lab.cex = 1.5, cex_main = 2)
+
+## Задание 3 ===============================================
+# Постройте танглграмму из дендрограмм,
+# полученных методом ближайшего соседа и методом Варда.
+
 
 
 # Раскраска дендрограмм ====================================
@@ -178,7 +204,7 @@ den_single_c <- color_labels(dend = den_single, col = cols)
 plot(den_single_c, horiz = TRUE)
 
 
-##
+## И небольшая демонстрация - дерево по генетическим данным ======
 webpage <- "http://evolution.genetics.washington.edu/book/primates.dna"
 primates.dna <- read.dna(webpage)
 d_pri <- dist.dna(primates.dna)
@@ -187,8 +213,22 @@ ph_pri <- as.phylo(hc_pri)
 plot.phylo(ph_pri)
 axisPhylo()
 
-
-##
 plot.phylo(ph_pri, type = "radial")
 
 ##### Самостоятельная работа ####
+# Проанализируйте данные об относительных обилиях фораминифер в пробах на Белом
+# море (данные из Golikova et al. 2020 из файла Golikova_etal_2020_cluster_data.csv).
+#
+# - Выберите и обоснуйте трансформацию данных и расстояние.
+#
+# - Постройте ординацию nMDS по относительным обилиям фораминифер: - цвет
+# значков --- растение-доминант, - форма значков --- точка сбора.
+#
+# - Постройте дендрограмму проб по сходству отн. обилий фораминифер. - оцените
+# при помощи кофенетической корреляции, какой метод аггрегации лучше,
+#
+# - Опишите получившиеся кластеры при помощи различных параметров: - ширина
+# силуэта - бутстреп-поддержка ветвлений
+
+foram <- read.table("data/Golikova_etal_2020_cluster_data.csv",
+                    sep = "\t", header = TRUE)
