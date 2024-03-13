6fd5f76d8939be601b68ab14b2c477d326b95f5c
Anastasia Lianguzova <anastasialianguzova@gmail.com>
2024-03-13 21:49:14 +0300

    before pca

--- 08_PCA_geom_morph.R
+++ 08_PCA_geom_morph.R
@@ -1,13 +1,166 @@
 # ---
-# title: "(Геометрическая) морфометрия"
+# title: "Анализ главных компонент"
 # subtitle: "Анализ и визуализация многомерных данных с использованием R"
-# author: Марина Варфоломеева
+# author: Марина Варфоломеева, Вадим Хайтов, Анастасия Лянгузова
 # ---
 
+# ## Тренировочные данные: медузы из реки Хоксбери ####
+# Данные из Lunn & McNeil, 1991
 
-# # Классический подход к морфометрии
+# Исходные данные
+jelly <- read.delim("data/jellyfish.csv")
+X_raw <- jelly[, 2:3]
+X <- scale() # Центрируем
+X_mat <- as.matrix()
+A <- t() %*%/(nrow()) # Матрица ковариаций
 
-# ## Пример: морфометрия черепах
+E <-            # Спектральное разложение
+U <-           # Собственные векторы
+Lambda <-     # Собственные числа
+# Координаты точек в новом пространстве
+Y <- X %*% U
+
+# график исходных данных
+
+gg_jelly_raw <- ggplot(as.data.frame(X_raw),
+                       aes(x = width, y = length)) +
+  geom_point(size = 2)
+
+# график в главных осях
+(gg_jelly_rotated <- ggplot(as.data.frame(Y),
+                            aes(x = V1, y = V2)) +
+    geom_point(size = 2) +
+    geom_hline(yintercept = 0) +
+    geom_vline(xintercept = 0) +
+    labs(x = "PC1", y = "PC2"))
+
+# графики вместе
+
+
+# график
+gg_rotated +
+  aes(colour = jelly$location) +
+  scale_color_brewer("Location", palette = "Set1") +
+  labs(title = "Результаты PCA",
+       x = paste0("PC1, ", round(Explained[1] * 100, 1), "%"),
+       y = paste0("PC2, ", round(Explained[2] * 100, 1), "%"))
+
+# ## Пример: Потребление белков в странах Европы с разными видами продуктов питания ####
+# Данные из Weber, 1973
+
+# ## Открываем данные
+protein <- read.table(file="data/protein.csv", sep="\t", dec=".", header=TRUE)
+protein$region <- factor(protein$region)
+rownames(protein) <- protein$country
+head(protein)
+
+# ## Делаем PCA  ####
+library(vegan)
+prot_pca <- rda(protein[, -c(1, 2)], scale = TRUE)
+op <- par(mar = c(5, 4, 0, 1) + 0.1)
+biplot(prot_pca)
+par(op)
+
+
+# ## Разбираемся с результатами PCA  ####
+tmp <- summary(prot_pca)
+
+# # 1. Сколько компонент нужно оставить? ----
+
+eigenvals(prot_pca) # собственные числа
+
+bstick(prot_pca) # ожидаемое по Broken Stick Model
+
+screeplot(prot_pca, type = "lines", bstick = TRUE) # График собственных чисел
+
+prop_expl <- eigenvals(prot_pca)/sum(eigenvals(prot_pca))
+cumsum(prop_expl*100)
+
+# # 2. Графики факторных н агрузок и ординации ----
+# ## Параметр `scaling`
+# Внимание! Координаты объектов или переменных можно получить в нескольких вариантах, отличающихся масштабом. От этого масштаба будет зависеть интерпретация.
+
+# Графики в vegan
+op <- par(mfrow = c(1, 2))
+# График факторных координат
+biplot(prot_pca, display = "sites", scaling = "sites")
+# График факторных нагрузок
+biplot(prot_pca, display = "species", scaling = "species")
+par(op)
+
+# ## Те же самые графики можно построить в ggplot2
+library(ggplot2)
+# Данные для графиков
+df_scores <- data.frame(protein[, 1:2],
+                        scores(prot_pca, display = "sites", choices = c(1, 2, 3), scaling = "sites"))
+## График ординации в ggplot
+
+p_scores <- ggplot(df_scores, aes(x = PC1, y = PC2, colour = region)) +
+  geom_text(aes(label = country)) +
+  coord_equal(xlim = c(-1.2, 1.2), ylim = c(-1.2, 1.2))
+
+## Данные для графика нагрузок
+df_load <- as.data.frame(scores(prot_pca, display = "species", choices = c(1, 2, 3), scaling = "species"))
+# поправки для размещения подписей
+df_load$hjust <- ifelse(df_load$PC1 >= 0, -0.1, 1)
+df_load$vjust <- ifelse(df_load$PC2 >= 0, -0.1, 1)
+library(grid) # для стрелочек
+ar <- arrow(length = unit(0.25, "cm"))
+## График нагрузок в ggplot
+p_load <- ggplot(df_load) +
+  geom_text(aes(x = PC1, y = PC2, label = rownames(df_load)),
+            size = 3, vjust = df_load$vjust, hjust = df_load$hjust) +
+  geom_segment(aes(x = 0, y = 0, xend = PC1, yend = PC2),
+               colour = "grey40", arrow = ar) +
+  coord_equal(xlim = c(-2, 2), ylim = c(-2, 2))
+
+
+
+# Все вместе
+library(cowplot)
+plot_grid(p_load, p_scores, align = "hv", rel_widths = c(0.4, 0.64))
+
+
+# # 3. Интерпретация компонент -----
+
+# Факторные нагрузки оценивают вклады переменных в изменчивость по главной компоненте
+scores(prot_pca, display = "species", choices = c(1, 2, 3), scaling = 0)
+
+
+
+
+# # Создание составных переменных при помощи PCA  ####
+
+
+# ## При помощи дисперсионного анализа можно проверить, различается ли значение первой главной компоненты ("Мясо -- злаки и орехи") между разными регионами Европы
+
+# Значения факторов (= факторные координаты)
+df <- data.frame(region = protein$region,
+  scores(prot_pca, display = "sites", choices = c(1, 2, 3), scaling = "sites"))
+mod <- lm(PC1 ~ region, data = df)
+anova(mod)
+
+# ## Проверка условий применимости дисперсионного анализа
+mod_diag <- fortify(mod)
+res_p <- ggplot(data = mod_diag, aes(x = .fitted, y = .stdresid)) + geom_point(aes(size = .cooksd)) + geom_hline(yintercept = 0) + geom_smooth(method="loess", se=FALSE)
+mean_val <- mean(mod_diag$.stdresid)
+sd_val <- sd(mod_diag$.stdresid)
+norm_p <- ggplot(mod_diag, aes(sample = .stdresid)) + geom_point(stat = "qq") + geom_abline(intercept = mean_val, slope = sd_val)
+plot_grid(res_p, norm_p, ncol = 2, rel_widths = c(0.55, 0.45))
+
+
+# ## График значений первой компоненты по регионам
+df$region <- reorder(df$region, df$PC1, FUN=mean)
+ggplot(df, aes(x = region, y = PC1, colour = region)) +
+  stat_summary(geom = "pointrange", fun.data = "mean_cl_boot", size = 1) +
+  theme(axis.text.x = element_text(angle = 30, vjust = 1, hjust = 1))
+
+# ## Пост-хок тест
+TukeyHSD(aov(mod))
+
+# # Классический подход к морфометрии ####
+
+# ## Пример: морфометрия черепах ####
 
 # Черепахи - единственные живые представители анапсид (череп не имеет височных окон). Морфология черепа важна для их систематики (Claude et al., 2004).
 # Данные - 24 разных измерения черепов черепах 122 ныне живущих пресноводных, морских и наземных видов и одного ископаемого. (Из Zuur et al. 2007)
@@ -70,9 +223,9 @@ legend("topright", legend = levels(turt$Environment3), bty = "n", pch = 21,
 par(op)
 
 
-# # Геометрическая морфометрия -----
+# # Геометрическая морфометрия  ####
 
-# ## Пример: Форма головы Апалачских саламандр рода _Plethodon_
+# ## Пример: Форма головы Апалачских саламандр рода _Plethodon_ ####
 #
 # _Plethodon jordani_ и _P.teyahalee_ встречаются вместе и раздельно.
 # В совместно обитающих популяциях меняется форма головы обоих видов. В разных группах популяций этот процесс параллельно приводит к одинаковым результатам. По-видимому, одной из причин параллельной эволюции может быть межвидовая конкуренция (Adams, 2004, 2010).
@@ -94,7 +247,7 @@ plotAllSpecimens(plethodon$land,links=plethodon$links)
 par(op)
 
 
-## Шаг 1. Выравниваем данные при помощи обобщенного прокрустова анализа ---
+## Шаг 1. Выравниваем данные при помощи обобщенного прокрустова анализа ----
 
 gpa <- gpagen(plethodon$land, print.progress = FALSE)
 plotAllSpecimens(gpa$coords,links=plethodon$links)
@@ -133,14 +286,13 @@ par(op)
 
 
 
-# ## Шаг 2. Создаем морфопространство
+# ## Шаг 2. Создаем морфопространство ----
+op <- par(mfrow = c(1, 2), mar = c(4, 4, 1, 1))
 ord <- gm.prcomp(gpa$coords)
 plot(ord, main = "PCA")
 
-#
-#
+
 # ## Можно раскрасить по группам
-op <- par(mar = c(4, 4, 1, 1))
 gp <- as.factor(paste(plethodon$species, plethodon$site)) # группа должна быть фактором
 # задаем соответствие цветов уровням фактора
 colvec <- c("Jord Allo" = "yellow2",
@@ -153,9 +305,9 @@ colvec <- colvec[match(gp, names(colvec))]
 plot(ord, bg = colvec, pch = 21, col = "grey20")
 # легенда
 legend("topright", legend = levels(gp),
-                   bty = "n", pch = 21,
-                   col = "grey20",
-                   pt.bg = levels(as.factor(colvec)))
+       bty = "n", pch = 21,
+       col = "grey20",
+       pt.bg = levels(as.factor(colvec)))
 par(op)
 
 
@@ -174,14 +326,14 @@ plot_shape_change <- function(ord, ref_shape, PC,
     plotRefToTarget(M1 = ref_shape, M2 = ord$shapes[[PC]]$max,
                     gridPars = gridPars, ...)
     par(op)
-    } else {
-     op <- par(mfrow = c(2, 1), mar = c(0, 0 , 0, 0))
-     plotRefToTarget(M1 = ref_shape, M2 = ord$shapes[[PC]]$max,
-                     gridPars = gridPars,  ...)
-     plotRefToTarget(M1 = ref_shape, M2 = ord$shapes[[PC]]$min,
-                     gridPars = gridPars, ...)
-     par(op)
-    }
+  } else {
+    op <- par(mfrow = c(2, 1), mar = c(0, 0 , 0, 0))
+    plotRefToTarget(M1 = ref_shape, M2 = ord$shapes[[PC]]$max,
+                    gridPars = gridPars,  ...)
+    plotRefToTarget(M1 = ref_shape, M2 = ord$shapes[[PC]]$min,
+                    gridPars = gridPars, ...)
+    par(op)
+  }
 }
 
 # ## Изменение формы вдоль главных компонент относительно средней формы
@@ -190,7 +342,8 @@ plot_shape_change(ord, ref_shape = gpa$consensus, PC = 1, links = plethodon$link
 plot_shape_change(ord, ref_shape = gpa$consensus, PC = 2, links = plethodon$links, method = "TPS", horiz = FALSE)
 
 # ## Можно нарисовать одновременно изменение формы вдоль обеих компонент и ординацию
-
+library(cowplot)
+library(gridGraphics)
 my_gridPar <- gridPar(tar.pt.size = 0.6, grid.lwd = 0.7)
 
 gg_pca <- plot_grid(
@@ -211,7 +364,7 @@ gg_pca <- plot_grid(
 gg_pca
 
 
-# # Эволюционные изменения формы
+# # Эволюционные изменения формы #####
 
 # ## Фило-морфопространство саламандр рода Plethodon
 data(plethspecies)
@@ -225,3 +378,4 @@ species_gpa <- gpagen(plethspecies$land) #GPA-alignment
 # Филоморфопространство
 pca_with_phylo <- gm.prcomp(species_gpa$coords, phy = plethspecies$phy)
 plot(pca_with_phylo, phylo = TRUE)
+
--- 08_PCA_tasks.R
+++ 08_PCA_tasks.R
@@ -7,14 +7,14 @@
 # 1) id - идентификационный номер пациента
 # 2) diagnosis - диагноз (M = malignant, B = benigh)
 # 3-12) среднее, 13-22) - стандартное отклонение и 23-32) - худшее значение следующих признаков:
-# a) radius (mean of distances from center to points on the perimeter)
-# b) texture (standard deviation of gray-scale values)
-# c) perimeter
-# d) area
-# e) smoothness (local variation in radius lengths)
+# a) radius (mean of distances from center to points on the perimeter; среднее расстояние от центра до точек на периметре)
+# b) texture (standard deviation of gray-scale values; стандартное отклонение значений серого)
+# c) perimeter (периметр)
+# d) area (площадь)
+# e) smoothness (local variation in radius lengths; локальное изменение длины радиуса)
 # f) compactness (perimeter^2 / area - 1.0)
-# g) concavity (severity of concave portions of the contour)
-# h) concave points (number of concave portions of the contour)
+# g) concavity (severity of concave portions of the contour; выраженность вогнутых участков по контору клетки)
+# h) concave points (number of concave portions of the contour; количество вогнутых участков по контору клетки)
 # i) symmetry
 # j) fractal dimension ("coastline approximation" - 1)
 #
@@ -30,4 +30,19 @@ brc <- read.csv("https://archive.ics.uci.edu/ml/machine-learning-databases/breas
 features <- c("radius", "texture", "perimeter", "area", "smoothness", "compactness", "concavity", "concave_points", "symmetry", "fractal_dimension")
 names(brc) <- c("id", "diagnosis", paste0(features,"_mean"), paste0(features,"_se"), paste0(features,"_worst"))
 
+# Геометрическая морфометрия тела рыб Cyprindon pecosensis.
+# - coords - координаты лендмарок, выравненные при помощи gpa
+# - CS - размер центроида
+# - Sex - пол ("F","M")
+# - Pop - популяция ("Marsh","Sinkhole")
+# Collyer, M.L., D.J. Sekora, and D.C. Adams. 2015. A method for analysis of phenotypic change for phenotypes described by high-dimensional data. Heredity. 115: 357-365.
 
+# Задание:
+# Сделайте PCA по выравненным координатам лендмарок.
+# Сколько изменчивости объясняют первые две главных компоненты?
+# Нарисуйте график главных компонент:
+# - раскрасьте точки в зависимости от пола и популяции рыб,
+# - приведите графики изменения формы вдоль главных компонент.
+
+library(geomorph)
+data("pupfish")
